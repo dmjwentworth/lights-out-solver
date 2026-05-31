60f6148fb1706d29af7da72d11878e0f01f4ba72
Dougal Maurice John Wentworth <dougalwentworth@gmail.com>
2026-05-31 17:51:45 +0100

    Used some numpy vectorisation to make the brute forcing about 3 times quicker

--- solver.py
+++ solver.py
@@ -1,7 +1,14 @@
 import numpy as np
 from tqdm import tqdm
 from itertools import combinations
-from utils import int_to_pos, update_grid, np_solved
+from utils import (
+    int_to_pos,
+    update_grid,
+    np_solved,
+    vec_solved,
+    vec_update_grid,
+    list_to_vec,
+    )
 
 
 def check_games_of_N_moves(grid, N):
@@ -34,7 +41,38 @@ def brute_force(grid):
     return None
 
 
-def solve(grid, fn=brute_force):
+def vec_check_games_of_N_moves(vec_grid, N):
+    for moves in tqdm(combinations(range(25), N)):
+        vec_grid_copy = np.copy(vec_grid)
+        moves = [int_to_pos(move) for move in moves]
+        for i, j in moves:
+            vec_update_grid(vec_grid_copy, i, j)
+        
+        if np.array_equal(vec_grid_copy, vec_solved):
+            print(f'\nFound a solution with {N} moves: {moves}')
+            return moves
+    
+    print(f'No solution found with {N} moves.')
+    return None
+
+
+def vec_brute_force(grid):
+    vec_grid = list_to_vec(grid)
+    if np.array_equal(vec_grid, vec_solved):
+        print('The grid is already solved.')
+        return []
+    
+    for N in range(1, 26):
+        print(f'\033[1;34mChecking for solutions with {N} moves...\033[0m')
+        moves = vec_check_games_of_N_moves(vec_grid, N)
+        if moves is not None:
+            return moves
+    
+    print('No solution found.')
+    return None
+
+
+def solve(grid, fn=vec_brute_force):
     return fn(grid)
 
 
--- utils.py
+++ utils.py
@@ -1,9 +1,58 @@
-import json
 import numpy as np
 
 np_solved = np.zeros((5, 5), dtype=int)
+vec_solved = -1 * np.ones((5, 5), dtype=int)
 solved = np_solved.tolist()
 colours = ['gray', 'blue']
+
+top_left = np.ones((5, 5), dtype=int)
+top_left[0, 0], top_left[0, 1], top_left[1, 0] = -1, -1, -1
+top_right = np.ones((5, 5), dtype=int)
+top_right[0, 4], top_right[0, 3], top_right[1, 4] = -1, -1, -1
+bottom_left = np.ones((5, 5), dtype=int)
+bottom_left[4, 0], bottom_left[4, 1], bottom_left[3, 0] = -1, -1, -1
+bottom_right = np.ones((5, 5), dtype=int)
+bottom_right[4, 4], bottom_right[4, 3], bottom_right[3, 4] = -1, -1, -1
+
+top_mid = np.ones((5, 5), dtype=int)
+top_mid[0, 1:4], top_mid[1, 2] = -1, -1
+bottom_mid = np.ones((5, 5), dtype=int)
+bottom_mid[4, 1:4], bottom_mid[3, 2] = -1, -1
+mid_left = np.ones((5, 5), dtype=int)
+mid_left[1:4, 0], mid_left[2, 1] = -1, -1
+mid_right = np.ones((5, 5), dtype=int)
+mid_right[1:4, 4], mid_right[2, 3] = -1, -1
+
+true_mid = np.ones((5, 5), dtype=int)
+true_mid[1, 2], true_mid[2, 1:4], true_mid[3, 2] = -1, -1, -1
+
+vec_toggle = {
+    (0, 0): top_left,
+    (0, 1): np.roll(top_mid, -1, axis=1),
+    (0, 2): top_mid,
+    (0, 3): np.roll(top_mid, 1, axis=1),
+    (0, 4): top_right,
+    (1, 0): np.roll(mid_left, -1, axis=0),
+    (1, 1): np.roll(true_mid, (-1, -1), axis=(0, 1)),
+    (1, 2): np.roll(true_mid, -1, axis=0),
+    (1, 3): np.roll(true_mid, (-1, 1), axis=(0, 1)),
+    (1, 4): np.roll(mid_right, -1, axis=0),
+    (2, 0): mid_left,
+    (2, 1): np.roll(true_mid, -1, axis=1),
+    (2, 2): true_mid,
+    (2, 3): np.roll(true_mid, 1, axis=1),
+    (2, 4): mid_right,
+    (3, 0): np.roll(mid_left, 1, axis=0),
+    (3, 1): np.roll(true_mid, (1, -1), axis=(0, 1)),
+    (3, 2): np.roll(true_mid, 1, axis=0),
+    (3, 3): np.roll(true_mid, (1, 1), axis=(0, 1)),
+    (3, 4): np.roll(mid_right, 1, axis=0),
+    (4, 0): bottom_left,
+    (4, 1): np.roll(bottom_mid, -1, axis=1),
+    (4, 2): bottom_mid,
+    (4, 3): np.roll(bottom_mid, 1, axis=1),
+    (4, 4): bottom_right
+}
 
 
 def int_to_pos(num):
@@ -20,6 +69,14 @@ def update_grid(grid, i, j, setup=False):
                 grid[x][y] = 1 - grid[x][y]
 
 
+def vec_update_grid(vec_grid, i, j, setup=False):
+    # Update the grid using vectorized operations
+    if setup:
+        vec_grid[i, j] *= -1
+    else:
+        vec_grid *= vec_toggle[(i, j)]
+
+
 def generate_random_grid():
     # Generate a random grid by applying random moves to the solved state
     np_grid = np.copy(np_solved)
@@ -32,4 +89,15 @@ def generate_random_grid():
         update_grid(np_grid, i, j)
     # return the list of moves and the resulting grid as a list of lists
     return moves, np_grid.tolist()
+
+
+def list_to_vec(grid):
+    np_grid = np.array(grid, dtype=int)
+    vec_grid = np.where(np_grid == 0, -1, 1)
+    return vec_grid
+
+
+def vec_to_list(vec_grid):
+    grid = np.where(vec_grid == -1, 0, 1)
+    return grid.tolist()
 
